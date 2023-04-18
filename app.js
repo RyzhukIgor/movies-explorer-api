3602043dc5615cde5267228f58bf831ec7584ef1
Igor Ryzhuk <ig.ryzhuk@yandex.ru>
2023-04-19 00:46:02 +0300

    деструктурировал переменные

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { PORT, DB_URL } = require('./utils/configurations');
 
 const app = express();
-const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
-  .then(() => {
-    console.log('Connected');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+mongoose.connect(DB_URL);
 
-app.listen(PORT, () => {
-  console.log(`Listing on port ${PORT}`);
-});
+app.listen(PORT);
