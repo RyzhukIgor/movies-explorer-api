8bed4131f4641938d831f50b1bf5c28b4ad7cea2
Igor Ryzhuk <ig.ryzhuk@yandex.ru>
2023-04-22 01:08:34 +0300

    проблема с cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const app = express();
 app.use(requestLogger);
 app.use(helmet());
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorLogger);
